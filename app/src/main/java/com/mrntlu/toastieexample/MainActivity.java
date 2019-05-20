0def5bb6135cbc54e026ffe4d7d7aa458486b53e
Burak Fidan <97burakfidan97@gmail.com>
2019-05-20 12:25:20 +0300

    Design bug fixed.

--- app/src/main/java/com/mrntlu/toastieexample/MainActivity.java
+++ app/src/main/java/com/mrntlu/toastieexample/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
-import com.mrntlu.toastie.ToastieActivity;
 
 public class MainActivity extends AppCompatActivity {
 
